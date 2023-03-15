bf96de73a321c3c65546a9a4a4241845c5715983
leromn <112072311+leromn@users.noreply.github.com>
2023-03-15 08:30:30 +0300

    just do it

--- jwt-project-main/app.js
+++ jwt-project-main/app.js
@@ -9,8 +9,10 @@ const Message = require("./model/collectionModel").Message;
 const messageSchema=require('./model/collectionModel').messageSchema;
 const auth = require("./middleware/auth");
 
-var myCors = require('cors')
-var app = express();
+var cors = require('cors')
+var app = express()
+
+app.use(cors())
 
 app.use(express.json({ limit: "50mb" }));
 
